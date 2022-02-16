1f955ba2d866840c82ff8cb6073b6d2f4e2e6dc5
sakthi s <velsakthis5301@gmail.com>
2022-02-16 12:29:17 +0530

    first commit

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="model">
+  <dialog open v-if="open">
+    <slot></slot>
+  </dialog>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ['open'],
+  emits: ['close'],
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.75);
+}
+
+dialog {
+  position: fixed;
+  top: 30vh;
+  width: 30rem;
+  left: calc(50% - 15rem);
+  margin: 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 12px;
+  padding: 1rem;
+  background-color: white;
+  z-index: 100;
+  border: none;
+  /* animation: model 0.3s ease-out forwards; */
+}
+
+.model-enter-active {
+  animation: model 0.3s ease-out;
+}
+
+.model-leave-active {
+  animation: model 0.3s ease-in reverse;
+}
+
+@keyframes model {
+  from{
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to{
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+  
+} 
+</style>
